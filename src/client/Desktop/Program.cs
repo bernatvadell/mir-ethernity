6a02ee29c7c73868b42cf0ad15ed25a2f957de58
bvadell <bvadell@intricom.es>
2020-04-25 19:28:48 +0200

    sample project cross platform

---
+++ src/client/Desktop/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Mir.Client
+{
+    /// <summary>
+    /// The main class.
+    /// </summary>
+    public static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            using (var game = new GameWindow())
+                game.Run();
+        }
+    }
+}
