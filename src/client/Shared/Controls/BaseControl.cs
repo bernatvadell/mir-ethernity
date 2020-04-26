e5a182d64e996fc147eadbc4861eb443f2c5e506
bvadell <bvadell@intricom.es>
2020-04-26 09:28:14 +0200

    add control collection to base control

--- src/client/Shared/Controls/BaseControl.cs
+++ src/client/Shared/Controls/BaseControl.cs
@@ -1,10 +1,21 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Runtime.CompilerServices;
 using System.Text;
 
 namespace Mir.Client.Controls
 {
-    public abstract class BaseControl
+    public abstract partial class BaseControl
     {
+        public uint ScreenX { get; set; }
+        public uint ScreenY { get; set; }
+
+        public ControlCollection Controls { get; private set; }
+        public BaseControl Parent { get; private set; }
+
+        public BaseControl()
+        {
+            Controls = new ControlCollection(this);
+        }
     }
 }
---
+++ src/client/Shared/Controls/ControlCollection.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Mir.Client.Controls
+{
+    public abstract partial class BaseControl
+    {
+        public class ControlCollection
+        {
+            private BaseControl _parent;
+            private List<BaseControl> _controls;
+
+            public ControlCollection(BaseControl parent)
+            {
+                _parent = parent ?? throw new ArgumentNullException(nameof(parent));
+                _controls = new List<BaseControl>();
+            }
+
+            public void Remove(BaseControl control)
+            {
+                _controls.Remove(control);
+                control.Parent = null;
+            }
+
+            public void Add(BaseControl control)
+            {
+                control.Parent?.Controls.Remove(control);
+                _controls.Add(control);
+                control.Parent = _parent;
+            }
+        }
+    }
+}
