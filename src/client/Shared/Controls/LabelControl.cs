60d965e9f57b0358df25214c52569cf18b2b5e0b
bvadell <bvadell@intricom.es>
2020-04-26 09:29:30 +0200

    create simple label control state

---
+++ src/client/Shared/Controls/LabelControl.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.Xna.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Mir.Client.Controls
+{
+    public class LabelControl : BaseControl
+    {
+        public string Text { get; set; }
+        public Color Color { get; set; }
+    }
+}
