a32fdde97fa942e87ce62d08afbe4ea2f416da5a
bvadell <bvadell@intricom.es>
2020-05-10 14:32:33 +0200

    Clean project

--- src/client/Android/GameActivity.cs
+++ src/client/Android/GameActivity.cs
@@ -6,31 +6,26 @@ using Mir.Client;
 
 namespace Mir.Client
 {
-    [Activity(Label = "Android"
-        , MainLauncher = true
-        , Icon = "@drawable/icon"
-        , Theme = "@style/Theme.Splash"
-        , AlwaysRetainTaskState = true
-        , LaunchMode = Android.Content.PM.LaunchMode.SingleInstance
-        , ScreenOrientation = ScreenOrientation.FullUser
-        , ConfigurationChanges = ConfigChanges.Orientation | ConfigChanges.Keyboard | ConfigChanges.KeyboardHidden | ConfigChanges.ScreenSize | ConfigChanges.ScreenLayout)]
-    public class GameActivity : Microsoft.Xna.Framework.AndroidGameActivity
-    {
-        protected override void OnCreate(Bundle bundle)
-        {
-            base.OnCreate(bundle);
+	[Activity(Label = "Android"
+		, MainLauncher = true
+		, Icon = "@drawable/icon"
+		, Theme = "@style/Theme.Splash"
+		, AlwaysRetainTaskState = true
+		, LaunchMode = Android.Content.PM.LaunchMode.SingleInstance
+		, ScreenOrientation = ScreenOrientation.FullUser
+		, ConfigurationChanges = ConfigChanges.Orientation | ConfigChanges.Keyboard | ConfigChanges.KeyboardHidden | ConfigChanges.ScreenSize | ConfigChanges.ScreenLayout)]
+	public class GameActivity : Microsoft.Xna.Framework.AndroidGameActivity
+	{
+		protected override void OnCreate(Bundle bundle)
+		{
+			base.OnCreate(bundle);
 
-            var g = GameBuilder.Create()
-                //.UseTextureGenerator<TextureGenerator>()
-                //.UseAssetLoader<AssetLoader>()
-                //.UseContext<WindowsGameContext>()
-                //.UseGamePadService<MouseGamePadService>()
-                //.UseGamePadService<KeyboardGamePadService>()
-                .Build();
-
-            SetContentView((View)g.Services.GetService(typeof(View)));
-            g.Run();
-        }
-    }
+			using (var game = new GameContext())
+			{
+				SetContentView((View)game.Services.GetService(typeof(View)));
+				game.Run();
+			}
+		}
+	}
 }
 
